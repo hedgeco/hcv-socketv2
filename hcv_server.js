95d2ccd53b8c480e87c0d4b3b207dae246594f65
Eddy Garcia <egarcia607@gmail.com>
2015-08-06 14:03:14 -0400

    setup node server

--- hcv_server.js
+++ hcv_server.js
@@ -1,9 +1,40 @@
-var http = require('http').createServer(handler)
+var http = require('http').createServer(handler),
+	io   = require('socket.io').listen(http).set('log level', 1);
  
 http.listen(8080);
-console.log('HCV Server listening on port 8080');
+console.log('HCV Server  listening on port 8080');
+ 
+var nicknames = {};
  
 function handler(req, res) {
   res.writeHead(200);
-  res.end('HCV Socket Server');
+  res.end();
 }
+ 
+io.sockets.on('connection', function (socket) {
+  socket.on('user message', function (msg) {
+    socket.broadcast.emit('user message', socket.nickname, msg);
+  });
+ 
+  socket.on('nickname', function (nick, fn) {
+    var i = 1;
+    var orignick = nick;
+    while (nicknames[nick]) {
+      nick = orignick+i;
+      i++;
+    }
+    fn(nick);
+    nicknames[nick] = socket.nickname = nick;
+    socket.broadcast.emit('announcement', nick + ' connected');
+    io.sockets.emit('nicknames', nicknames);
+  });
+ 
+  socket.on('disconnect', function () {
+    if (!socket.nickname) return;
+ 
+    delete nicknames[socket.nickname];
+    socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
+    socket.broadcast.emit('nicknames', nicknames);
+  });
+ 
+});
